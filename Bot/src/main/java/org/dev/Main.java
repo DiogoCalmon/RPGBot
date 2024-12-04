e285cd5f03b4e7d83a3c3304868223a8c243f6da
DiogoCalmon <diogocalmons@gmail.com>
2024-12-04 07:36:14 -0300

    trying again

--- Bot/src/main/java/org/dev/Main.java
+++ Bot/src/main/java/org/dev/Main.java
@@ -1,7 +1,13 @@
 package org.dev;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.util.EnumSet;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JDA kit = JDABuilder.createDefault("", EnumSet.allOf(GatewayIntent.class)).build();
     }
 }
